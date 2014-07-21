bd2556508a7b284b47004e5af3b8d2a7b7fdef95
divine <divine@divine-Satellite-C650>
2014-07-21 17:48:36 +0300

    firstmodule

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+  before_filter :authenticate_user!, :only => [:index, :new]
+
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindMyShop::Application.config.secret_token = '7aa8dae18426bfca5fd750c80ff642eb6efd94b3141bf9eccdef41c67f4053099404a33661a1a36e7896733f65103c7adfec57a04ec99dbb02279f7379c2ad1e'
