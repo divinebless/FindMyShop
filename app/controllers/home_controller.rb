11042532c486d799a89506bf7e12c404d34118c0
divine <divine@divine-Satellite-C650>
2014-08-01 11:46:38 +0300

    personalize accounts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,73 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :only => [:index, :new]
 
   def index
-    @user = User.all
+    @user = User.where(:id => current_user.id)
   end
-  
+
+  def show
+    @profile = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @profile }
+    end
+  end
+ 
+  def new
+    @profile = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @profile }
+    end
+  end
+
+  def edit
+    @profile = User.find(params[:id])
+  end
+
+  # POST /profiles
+  # POST /profiles.json
+  def create
+    @profile = User.new(params[:user])
+
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
+        format.json { render json: @profile, status: :created, location: @profile }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /profiles/1
+  # PUT /profiles/1.json
+  def update
+    @profile = User.find(params[:id])
+
+    respond_to do |format|
+      if @profile.update_attributes(params[:user])
+        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /profiles/1
+  # DELETE /profiles/1.json
+  def destroy
+    @profile = User.find(params[:id])
+    @profile.destroy
+
+    respond_to do |format|
+      format.html { redirect_to profiles_url }
+      format.json { head :no_content }
+    end
+  end
+
 end
